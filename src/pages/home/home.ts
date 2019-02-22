cb53757eb9cc1f45d9ea9eaec3a1a1a24281d254
Broivula <elias.koivula1@hotmail.com>
2019-02-22 13:49:07 +0200

    initial getting works

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MediaProvider } from "../../providers/media/media";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private data: MediaProvider
+  ) {}
 
+
+  getList() {
+    this.data.getShoppingList();
   }
 
 }
